907ae02d50a411240cdc637a672107da02b56b76
deb-gama <gama.debora1@gmail.com>
2023-08-12 22:23:19 -0300

    feat(tdd-python-practice): adding the first rule for quality drop on check sell in date to rupdate quality method

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -3,21 +3,31 @@ class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
+    # SELL'_IN RULES
+    # sell_in value must be decremented each time that update quality method was called OK
+    # Sulfura no need sell_in value -- sell_in == 0 OK
 
-    # sell_in value must be decremented each time that update quality method was called
-    # Sulfura no need sell_in value -- sell_in == 0
+    # QUALITY RULES
+    # Quando a data de venda do item tiver passado, a qualidade (`quality`) do item diminui duas vezes mais rapido.
+    # A qualidade (`quality`) do item não pode ser negativa
+    # O "Queijo Brie envelhecido" (`Aged Brie`), aumenta sua qualidade (`quality`) em `1` unidade a medida que envelhece.
     def sell_in_drop(self, item):
-
         if "Sulfuras" in item.name:
             item.sell_in = 0
         else: 
             item.sell_in -= 1
+            
+        return item
+    
+    def check_sell_in_date_to_update_quality(self, item):
+        item.quality -= 1
 
         return item
 
     def update_quality(self):
         for item in self.items:
             self.sell_in_drop(item)
+            self.check_sell_in_date_to_update_quality(item)
             # if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
             #     if item.quality > 0:
             #         if item.name != "Sulfuras, Hand of Ragnaros":
