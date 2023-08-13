5585f2bf1b47d0d3c6f9dac7a56375e7bb987b8d
deb-gama <gama.debora1@gmail.com>
2023-08-13 01:06:30 -0300

    test(tdd-python-practice): fixing tests to consider the gilded rose legendary items refactory

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import unittest
-from gilded_rose import Item, GildedRose
-from gilded_rose_test_base import GildedTestBase
+from python.gilded_rose import Item, GildedRose
+from tests.gilded_rose_test_base import GildedTestBase
 
+LEGENDARY_ITEMS = ['Sulfuras, Hand of Ragnaros']
 class GildedRoseTest(GildedTestBase):
     
     def test_sell_in_must_be_decremented_when_update_quality_method_was_called(self):
@@ -12,38 +13,40 @@ class GildedRoseTest(GildedTestBase):
         for _ in range(days):
             gilded.update_quality()
         
-        expected_result = [8,0,3,0,0,13,8,3,1]
+        expected_result = [8,0,3,13,8,3,1]
 
         with self.subTest(expected_result = expected_result):
             result = []
             for item in self.items:
-                result.append(item.sell_in)
+                if item.name not in LEGENDARY_ITEMS:
+                    result.append(item.sell_in)
             
             self.assertEqual(result, expected_result)
 
 
-    def test_sell_in_always_must_be_zero_if_item_is_sulfuras(self):
-        gilded = self.mocked_gilded
-        expected_result = 0
+    # def test_sell_in_always_must_be_zero_if_item_is_sulfuras(self):
+    #     gilded = self.mocked_gilded
+    #     expected_result = 0
 
-        gilded.update_quality()
+    #     gilded.update_quality()
 
-        with self.subTest(expected_result = expected_result):
-            for item in self.items:
-                if 'Sulfuras' in item.name:
-                    self.assertEqual(item.sell_in,expected_result)
+    #     with self.subTest(expected_result = expected_result):
+    #         for item in self.items:
+    #             if 'Sulfuras' in item.name:
+    #                 self.assertEqual(item.sell_in,expected_result)
 
 
     def test_quality_must_be_decremented_once_when_update_quality_method_was_called(self):
         gilded = self.mocked_gilded
         gilded.update_quality()
         
-        expected_result = [19,0,6,78,78,19,48,48,5]
+        expected_result = [19,0,6,19,48,48,5]
 
         with self.subTest(expected_result = expected_result):
             result = []
             for item in self.items:
-                result.append(item.quality)
+                if item.name not in LEGENDARY_ITEMS:
+                    result.append(item.quality)
             
             self.assertEqual(result, expected_result)
 
@@ -51,12 +54,13 @@ class GildedRoseTest(GildedTestBase):
         gilded = self.mocked_gilded
         gilded.update_quality()
         
-        expected_result = [19,-0,6,78,78,19,48,48,5]
+        expected_result = [19,-0,6,19,48,48,5]
 
         with self.subTest(expected_result = expected_result):
             result = []
             for item in self.items:
-                result.append(item.quality)
+                if item.name not in LEGENDARY_ITEMS:
+                    result.append(item.quality)
             
             self.assertEqual(result, expected_result)
 
@@ -67,14 +71,34 @@ class GildedRoseTest(GildedTestBase):
         for _ in range(days):
             gilded.update_quality()
         
-        expected_result = [17,0,4,74,74,17,46,46,2]
+        expected_result = [17,0,4,17,46,46,2]
 
         with self.subTest(expected_result = expected_result):
             result = []
             for item in self.items:
+                if item.name not in LEGENDARY_ITEMS:
+                    result.append(item.quality)
+            
+            self.assertEqual(result, expected_result)
+
+    def test_brie_must_have_the_quality_incremented(self):
+        days = 2
+        gilded = self.mocked_gilded
+
+        for _ in range(days):
+            gilded.update_quality()
+        
+        expected_result = [18,2,5,17,46,46,2]
+
+        with self.subTest(expected_result = expected_result):
+            result = []
+  
+            for item in self.items:
                 result.append(item.quality)
             
             self.assertEqual(result, expected_result)
+
+            print(result)
 
 
 
@@ -83,3 +107,5 @@ class GildedRoseTest(GildedTestBase):
         
 if __name__ == '__main__':
     unittest.main()
+
+    
