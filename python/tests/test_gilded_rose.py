2a1d9a7336f9a0304cd233f044ebb1628bcc6869
deb-gama <gama.debora1@gmail.com>
2023-08-12 22:17:00 -0300

    test(tdd-python-practice): cerating quality decremented method test to failed in firts step of the red green refactor flow

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -24,7 +24,7 @@ class GildedRoseTest(GildedTestBase):
             self.assertEqual(result, expected_result)
 
 
-    def test_sell_in_must_be_zero_if_item_is_sulfuras(self):
+    def test_sell_in_always_must_be_zero_if_item_is_sulfuras(self):
         gilded = self.mocked_gilded
         expected_result = 0
 
@@ -34,6 +34,22 @@ class GildedRoseTest(GildedTestBase):
             for item in self.items:
                 if 'Sulfuras' in item.name:
                     self.assertEqual(item.sell_in,expected_result)
+
+    def test_quality_must_be_decremented_when_update_quality_method_was_called(self):
+        days = 2
+        gilded = self.mocked_gilded
+
+        for _ in range(days):
+            gilded.update_quality()
+        
+        expected_result = [19,-1,6,79,79,19,48,48,5]
+
+        with self.subTest(expected_result = expected_result):
+            result = []
+            for item in self.items:
+                result.append(item.quality)
+            
+            self.assertEqual(result, expected_result)
 
 
 
