68cc413775844e983957636725af88d132c1ecd7
deb-gama <gama.debora1@gmail.com>
2023-08-13 01:05:43 -0300

    refactor(tdd-python-practice):refactoring gilded rose methods to exclude legendary items for the generic methods because they dont need to have sell in or quality values

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+
+LEGENDARY_ITEMS = ['Sulfuras, Hand of Ragnaros']
+LEGENDARY_ITEMS_STATUS = "Bitch I'm legendary!"
 class GildedRose(object):
 
     def __init__(self, items):
@@ -9,14 +12,17 @@ class GildedRose(object):
 
     # QUALITY RULES
     # Quando a data de venda do item tiver passado, a qualidade (`quality`) do item diminui duas vezes mais rapido. (if sell_in == 0, quality -= 2) OK
-    # A qualidade (`quality`) do item não pode ser negativa
+    # A qualidade (`quality`) do item não pode ser negativa OK
+
+    # SPECIAL QUALITY UPDATE CONDITIONS
     # O "Queijo Brie envelhecido" (`Aged Brie`), aumenta sua qualidade (`quality`) em `1` unidade a medida que envelhece.
+    # O item "Sulfuras" (`Sulfuras`), por ser um item lendário, não precisa ter uma data de venda (`SellIn`) e sua qualidade (`quality`) não precisa ser diminuida.
+    # O item "Entrada para os Bastidores" (`Backstage Passes`), assim como o "Queijo Brie envelhecido", aumenta sua qualidade (`quality`) a medida que o dia da venda (`SellIn`) se aproxima;
+    
+    # Sulfuras its legendary, bitch... a update não muda seus valores de sell_in e quality que podem ser n/a - formatados em um método
+    
     def sell_in_drop(self, item):
-        if "Sulfuras" in item.name:
-            item.sell_in = 0
-        else: 
-            item.sell_in -= 1
-            
+        item.sell_in -= 1
         return item
     
     def check_sell_in_date_to_update_quality(self, item):
@@ -33,11 +39,20 @@ class GildedRose(object):
         
         return item
 
+    def format_legendary_items(self,item):
+        item.sell_in = LEGENDARY_ITEMS_STATUS
+        item.quality = LEGENDARY_ITEMS_STATUS
+        return item
+    
+
     def update_quality(self):
         for item in self.items:
-            self.sell_in_drop(item)
-            self.check_sell_in_date_to_update_quality(item)
-            self.format_quality_value(item)
+            if item.name not in LEGENDARY_ITEMS:
+                self.sell_in_drop(item)
+                self.check_sell_in_date_to_update_quality(item)
+                self.format_quality_value(item)
+            else:
+                self.format_legendary_items(item)
             # if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
             #     if item.quality > 0:
             #         if item.name != "Sulfuras, Hand of Ragnaros":
